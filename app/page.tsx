f66e294e5026202de8c4b8b1dd1aa3bd8a06e3c6
surajarya130 <surajarya130@gmail.com>
2025-04-17 11:37:05 +0530

    Fixed - Pokemon App

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,6 @@ export default function HomePage() {
 
   const { filteredList, loading } = usePokemonList(selectedType, searchTerm);
 
-  console.log(filteredList, "srj filter");
-
   return (
     <main className="max-w-4xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Pokémon Search</h1>
